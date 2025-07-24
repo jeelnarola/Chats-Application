4187bb22be17005c3f3a7726d9596f472c2bb649
jeelnarola <narolajeel29@gmail.com>
2025-07-24 10:54:49 +0530

    Create A Route in Front-End

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,6 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const getUserInuserid = async(req,res)=>{
     try {
@@ -8,5 +10,51 @@ export const getUserInuserid = async(req,res)=>{
     } catch (error) {
          console.log("message Controller from getUserInuserid Function Error :- ", error.message);
         res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export const getMessage = async(req,res)=>{
+    try {
+        const {id:userToChatId} = req.params
+        const myId = req.user._id;
+
+        const message = await Message.find({
+            $or:[
+                {senderId:myId,receiverId:userToChatId},
+                {senderId:userToChatId,receiverId:myId},
+
+            ]
+        })
+        res.status(200).json({message:message})
+    } catch (error) {
+       console.log("message Controller from getMessage Function Error :- ", error.message);
+        res.status(500).json({ error: "Internal Server Error" }); 
+    }
+}
+export const sendMessage = async(req,res)=>{
+    try {
+            const {text,image} = req.body;
+            const {id:receiverId} = req.params
+            let imageurl;
+
+            if(image){
+                const uplodResponse = await cloudinary.uploader.upload(image);
+                imageurl = uplodResponse.secure_url;
+            }
+
+            const newMessage = new Message({
+                senderId,
+                receiverId,
+                text,
+                image:imageurl
+            })
+            await newMessage.save();
+
+            // RealTime Functionality Add soket.io
+
+             res.status(200).json({message:newMessage})
+    } catch (error) {
+        console.log("message Controller from getMessage Function Error :- ", error.message);
+        res.status(500).json({ error: "Internal Server Error" }); 
     }
 }
--- backend/src/routers/message.route.js
+++ backend/src/routers/message.route.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { getUserInuserid } from "../controllers/message.controller.js";
+import { getMessage, getUserInuserid, sendMessage } from "../controllers/message.controller.js";
 export  const messageRoute = Router()
 
 messageRoute.get("/users",protectRoute,getUserInuserid)
+messageRoute.get("/:id",protectRoute,getMessage)
+messageRoute.post("/send/:id",protectRoute,sendMessage)
+
+
